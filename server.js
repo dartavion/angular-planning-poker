5b98b87a4a75c671c929fcc6a095fc7d4860ef47
Dartavion <dartavion@icloud.com>
2016-05-30 20:19:45 -0400

    static site

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static('./dist/'))
 app.use('/*', express.static('./dist/index.html'));
-// app.get('/', function(request, response) {
-//   response.send('Hello World!')
-// })
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + __dirname + app.get('port'))
