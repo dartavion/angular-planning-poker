41e53df5d0e83a9797169770b7a59b122b33b28c
Dartavion <dartavion@icloud.com>
2016-05-30 21:16:57 -0400

    hrmf

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-const path = require('path')
-const express = require('express')
+var express = require('express')
+var app = express()
 
-module.exports = {
-  app: function () {
-    const app = express()
-    const indexPath = path.join(__dirname, './dist/index.html')
-    const publicPath = express.static(path.join(__dirname, '../public'))
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static('./dist/'))
+app.use('/*', express.static('./dist/index.html'));
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-    app.use('/public', publicPath)
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
-
-    return app
-  }
-}
-
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + __dirname + app.get('port'))
+})
